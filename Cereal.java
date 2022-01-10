cbbee7a3b0950ed9c8537a596259571676ed3efc
Ty W <71028905+s10486025@users.noreply.github.com>
2022-01-10 09:20:34 -0500

    v0.1

--- Cereal.java
+++ Cereal.java
@@ -1,1 +1,11 @@
-// Ty Whitecotten 01/10/22, AP Data Lab 2022, v0.0
+// Ty Whitecotten 01/10/22, AP Data Lab 2022, v0.1
+
+public class Cereal 
+{
+
+    private String cerealName;
+    private double fiber;
+    private double cups;
+    private double fiberPerCup; 
+
+}
